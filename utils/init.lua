091df40579863d0118c0897d988dab1460d754a6
YuchengMo <YuchengMo@boyaa.com>
2019-06-17 11:13:49 +0800

    fix: 新增性能分析代码
    详细信息:
    
    # 提交类型字段
    # feat：新功能（feature）
    # fix： 修补bug
    # docs：文档（documentation）
    # refactor：重构（即不是新增功能，也不是修改bug的代码变动）
    # test：增加测试

--- utils/init.lua
+++ utils/init.lua
@@ -1,8 +1,10 @@
 -- @Author: myc
 -- @Date:   2019-06-17 10:09:02
 -- @Last Modified by   YuchengMo
--- @Last Modified time 2019-06-17 10:25:16
+-- @Last Modified time 2019-06-17 11:10:50
 
 require("utils.functions");
 import(".dump")
 dumpToFile = import(".dumpToFile").dumpToFile;
+
+newProfiler = import(".profiler");
